d9a918279b9cc81f5ee65210f9b5315a1e42a78d
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-06 18:42:46 +0200

    added SimpleEntry

---
+++ java/util/AbstractMap/SimpleEntry.js
@@ -0,0 +1,68 @@
+qx.Class.define( 'java.util.AbstractMap.SimpleEntry', {
+    extend: java.util.Map.Entry,
+    implement: [java.io.Serializable],
+
+    /* Note : this class is not static since unlike in Java, it is not represented as an inner class */
+
+    construct: function () {
+        var args = Array.prototype.slice.call( arguments );
+        switch( args.length ) {
+            case 1:
+                this.assertInterface( args[0], java.util.Map.Entry );
+                this.__key = args[0].getKey();
+                this.__value = args[0].getValue();
+                break;
+            case 2:
+                this.__key = args[0];
+                this.__value = args[1];
+                break;
+            default:
+                /* TODO throw error */
+                break;
+        }
+    },
+
+    statics: {
+        serialVersionUID: 1
+    },
+
+    members: {
+        __key: undefined,
+        __value: undefined,
+
+        __eq: function (obj1, obj2) {
+            return obj1 === null ? obj2 === null : obj1.equals( obj2 );
+        },
+
+        getKey: function () {
+            return this.__key;
+        },
+
+        getValue: function () {
+            return this.__value;
+        },
+
+        setValue: function (value) {
+            var oldValue = this.__value;
+            this.__value = value;
+            return oldValue;
+        },
+
+        equals: function (other) {
+            if( !(other instanceof java.util.Map.Entry) ) {
+                return false;
+            }
+
+            return __eq( this.__key, other.getKey() ) && eq( this.__value, other.getValue() );
+        },
+
+        hashCode: function () {
+            return (this.__key === null ? 0 : this.__key.hashCode()) ^
+                (this.__value === null ? 0 : this.__value.hashCode());
+        },
+
+        toString: function () {
+            return this.__key + '=' + this.__value;
+        }
+    }
+} );
