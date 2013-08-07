9b4cf7bf26164bc278fbdca2503391d3d2587b4e
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-07 16:22:40 +0200

    added Object methods

--- src/java/lang/Object.js
+++ src/java/lang/Object.js
@@ -1,3 +1,45 @@
 qx.Class.define( 'java.lang.Object', {
-    /* TODO */
+    members: {
+        getClass: function () {
+            return this.name;
+        },
+
+        hashCode: function () {
+            // TODO
+            throw new java.lang.UnsupportedOperationException();
+        },
+
+        equals: function (other) {
+            return this === other;
+        },
+
+        clone: function () {
+            // TODO
+            throw new java.lang.UnsupportedOperationException();
+        },
+
+        toString: function () {
+            // TODO not exact implementation
+            return this.getClass().name + '@' + this.hashCode();
+        },
+
+        notify: function () {
+            // TODO
+            throw new java.lang.UnsupportedOperationException();
+        },
+
+        notifyAll: function () {
+            // TODO
+            throw new java.lang.UnsupportedOperationException();
+        },
+
+        wait: function () {
+            // TODO
+            throw new java.lang.UnsupportedOperationException();
+        },
+
+        finalize: function () {
+            throw new java.lang.UnsupportedOperationException();
+        }
+    }
 } );
