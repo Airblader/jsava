10ef2c8a12164c9c61b510b4d41d6b48e66a5caf
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-07 21:46:49 +0200

    Make UnsupportedOperationException extend from RuntimeException

--- src/java/lang/UnsupportedOperationException.js
+++ src/java/lang/UnsupportedOperationException.js
@@ -1,21 +1,11 @@
 qx.Class.define( 'java.lang.UnsupportedOperationException', {
-    // TODO fix inheritance to RuntimeException
-    extend: java.lang.Object,
+    extend: java.lang.RuntimeException,
 
     construct: function () {
-    // TODO fix when above is fixed
-    //    this.base( arguments );
-
-        this.message = arguments[0] || null;
-        this.cause = arguments[1] || null;
+        this.base( arguments );
     },
 
-    members: {
-        message: null,
-        cause: null,
-
-        toString: function () {
-            return 'UnsupportedOperationException: ' + this.message + ' <' + this.cause + '>';
-        }
+    statics: {
+        serialVersionUID: 1
     }
 } );
