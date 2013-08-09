85edb275c064726666d7d84ef208491e1647d799
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-09 11:12:24 +0200

    add jsava.util.Iterator

---
+++ src/jsava/util/Iterator.js
@@ -0,0 +1,14 @@
+qx.Interface.define( 'jsava.util.Iterator', {
+    extend: jsava.lang.Object,
+
+    members: {
+        hasNext: function () {
+        },
+
+        next: function () {
+        },
+
+        remove: function () {
+        }
+    }
+} );
