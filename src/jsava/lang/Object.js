d77e3f7ae75f7181f0578a7869ea772f5a340d62
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-11 22:37:31 +0200

    new hashCode implementation

--- src/jsava/lang/Object.js
+++ src/jsava/lang/Object.js
@@ -1,26 +1,28 @@
 qx.Class.define( 'jsava.lang.Object', {
     extend: qx.core.Object,
 
-    statics: {
-        __hashCodeCache: -9007199254740992
-    },
-
     members: {
-        __hashCode: undefined,
-
         getClass: function () {
             return this.name;
         },
 
         hashCode: function () {
-            // TODO use qx's internal toHashCode() ?
-            if( typeof this.__hashCode === 'undefined' ) {
-                // TODO this is incorrect as objects that are equal will not report the same hashCode
-                //this.__hashCode = jsava.lang.Object.__hashCodeCache++;
-                return 0;
+            var hashCode = 0;
+
+            // TODO if hashCode() is available on property, use it
+            for( var property in this ) {
+                if( !this.hasOwnProperty( property )
+                    || typeof this[property] === 'undefined' || !this[property].toString ) {
+                    continue;
+                }
+
+                var temp = this[property].toString();
+                for( var i = 0; i < temp.length; i++ ) {
+                    hashCode = (31 * hashCode + temp.charCodeAt( i )) << 0;
+                }
             }
 
-            return this.__hashCode;
+            return hashCode;
         },
 
         equals: function (other) {
