ddde5874a3332085a3009ce28a37c0d6e76740c4
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-12 19:47:46 +0200

    added some comments

--- src/jsava/util/Map.js
+++ src/jsava/util/Map.js
@@ -1,14 +1,28 @@
 qx.Interface.define( "jsava.util.Map", {
     members: {
+        /**
+         * @return {Integer}
+         */
         size: function () {
         },
 
+        /**
+         * @return {Boolean}
+         */
         isEmpty: function () {
         },
 
+        /**
+         * @param {jsava.lang.Object} key
+         * @return {Boolean}
+         */
         containsKey: function (key) {
         },
 
+        /**
+         * @param {jsava.lang.Object} value
+         * @return {Boolean}
+         */
         containsValue: function (value) {
         },
 
@@ -29,18 +43,34 @@ qx.Interface.define( "jsava.util.Map", {
 
         },
 
+        /**
+         * @return {jsava.util.Set}
+         */
         keySet: function () {
         },
 
+        /**
+         * @return {jsava.util.Collection}
+         */
         values: function () {
         },
 
+        /**
+         * @return {jsava.util.Set}
+         */
         entrySet: function () {
         },
 
+        /**
+         * @param other
+         * @return {Boolean}
+         */
         equals: function (other) {
         },
 
+        /**
+         * @return {Integer}
+         */
         hashCode: function () {
         }
     }
