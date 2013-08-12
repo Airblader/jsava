fa74f4f16f2a63c4d54f797875eca80768f04e4f
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-12 23:57:45 +0200

    revert helper method to simple version

--- src/jsava/JsavaUtils.js
+++ src/jsava/JsavaUtils.js
@@ -8,27 +8,11 @@ qx.Class.define( 'jsava.JsavaUtils', {
          * Creates an anonymous class and returns it.
          * @returns {*}
          */
-        createAnonymousClass: function () {
-            var args = Array.prototype.slice.call( arguments ),
-                clazzName, config,
-                defaultClazzName = 'Anonymous';
-
-            switch( args.length ) {
-                case 1:
-                    clazzName = defaultClazzName;
-                    config = args[0];
-                    break;
-                case 2:
-                    clazzName = args[0];
-                    config = args[1];
-                    break;
-            }
+        createAnonymousClass: function (config) {
+            var clazzName = 'Anonymous';
 
             var clazz = qx.Class.define( clazzName, config );
-
-            if( clazzName === defaultClazzName ) {
-                qx.Class.undefine( clazzName );
-            }
+            qx.Class.undefine( clazzName );
 
             return clazz;
         }
