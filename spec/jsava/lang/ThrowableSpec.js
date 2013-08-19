3086ec56a31c49ad008295750eb5b98e046a9aa6
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-19 19:18:53 +0200

    added more tests for Throwables

--- spec/jsava/lang/ThrowableSpec.js
+++ spec/jsava/lang/ThrowableSpec.js
@@ -5,8 +5,30 @@ describe( 'Throwable', function () {
         it( 'can initialize an empty object', function () {
             var t = new Throwable();
 
-            expect( t.__cause ).toBe( t );
-            expect( t.__detailMessage ).toBe( null );
+            expect( t.getCause() ).toBe( t );
+            expect( t.getMessage() ).toBe( null );
         } );
+
+        it( 'can have a detail message', function () {
+            expect( new Throwable( 'Detail Message' ).getMessage() ).toBe( 'Detail Message' );
+        } );
+
+        it( 'can have a cause', function () {
+            var cause = new Throwable();
+            expect( new Throwable( cause ).getCause() ).toBe( cause );
+        } );
+
+        it( 'can have a detail message and a cause', function () {
+            var cause = new Throwable(),
+                t = new Throwable( 'Detail Message', cause );
+
+            expect( t.getMessage() ).toBe( 'Detail Message' );
+            expect( t.getCause() ).toBe( cause );
+        } );
+    } );
+
+    it( 'toString() returns proper string representation', function () {
+        var t = new Throwable( 'Some message' );
+        expect( t.toString() ).toBe( 'jsava.lang.Throwable: Some message' );
     } );
 } );
