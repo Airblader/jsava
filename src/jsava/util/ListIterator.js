42a40ec920edba3a07d37c94f0c996f89097ef81
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-20 07:36:43 +0200

    added ListIterator interface

---
+++ src/jsava/util/ListIterator.js
@@ -0,0 +1,44 @@
+qx.Interface.define( 'jsava.util.ListIterator', {
+    extend: jsava.util.Iterator,
+
+    members: {
+        /**
+         * @return {Boolean}
+         */
+        hasNext: function () {
+        },
+
+        next: function () {
+        },
+
+        /**
+         * @return {Boolean}
+         */
+        hasPrevious: function () {
+        },
+
+        previous: function () {
+        },
+
+        /**
+         * @return {Number}
+         */
+        nextIndex: function () {
+        },
+
+        /**
+         * @return {Number}
+         */
+        previousIndex: function () {
+        },
+
+        remove: function () {
+        },
+
+        set: function (element) {
+        },
+
+        add: function (element) {
+        }
+    }
+} );
