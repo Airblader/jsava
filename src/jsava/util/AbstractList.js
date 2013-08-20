37d4a437ff57b847b65cbe239af9b8cb363be1b0
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-20 16:34:40 +0200

    use Itr class

--- src/jsava/util/AbstractList.js
+++ src/jsava/util/AbstractList.js
@@ -93,7 +93,7 @@ qx.Class.define( 'jsava.util.AbstractList', {
         },
 
         iterator: function () {
-            // TODO return new Itr();
+            return new this.Itr();
         },
 
         listIterator: function () {
