7bf10627f01c684ff030126c13d7ab33a04c6bcc
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-23 15:49:16 +0200

    added Arrays#hashCode

--- test/jsava/util/ArraysUnitTest.js
+++ test/jsava/util/ArraysUnitTest.js
@@ -16,4 +16,21 @@ describe( 'Arrays', function () {
             expect( Arrays.copyOf( sourceArray, 5 ) ).toEqual( [0, 1, 2, null, null] );
         } );
     } );
+
+    describe( 'hashCode()', function () {
+        it( 'can compute the hash of several objects', function () {
+            var map = new jsava.util.HashMap();
+            map.put( 1, 10 );
+            map.put( 10, 42 );
+
+            var list = new jsava.util.ArrayList();
+            list.add( 10 );
+            list.add( 42 );
+
+            var obj = new jsava.lang.Object();
+            obj.__fake_property = 42;
+
+            expect( typeof Arrays.hashCode( [map, list, obj] ) ).toBe( 'number' );
+        } );
+    } );
 } );
