bf84ae269cb64cabb31582a5690f24a66d2fdc13
Robert Himmelmann <robert.himmelmann@tngtech.com>
2013-08-25 15:01:34 +0200

    Added entry class

--- src/jsava/util/TreeMap.js
+++ src/jsava/util/TreeMap.js
@@ -7,13 +7,38 @@ qx.Class.define( 'jsava.util.TreeMap', {
 
     statics: {
         /** @private */
-        serialVersionUID: 1
+        serialVersionUID: 1,
+        Entry: qx.Class.define( 'jsava.util.TreeMap.Entry', {
+            extend: jsava.lang.Object,
+            implement: [jsava.util.Map.Entry],
+
+            construct: function (key, value, parent) {
+                this.key = key;
+                this.value = value;
+                this.parent = parent;
+            },
+
+            members: {
+                key: undefined,
+                value: undefined,
+                left: undefined,
+                right: undefined,
+                parent: undefined,
+                color: 'black'
+            }
+        } )
     },
 
     members: {
+        root: undefined,
+        _size: 0,
+        modCount: 0,
+        comparator: undefined,
+
         add: undefined,
         iterator: undefined,
         size: undefined,
         isEmpty: undefined
     }
-} );
+} )
+;
