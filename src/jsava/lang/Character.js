d018c423d9c60bc7190d21def758b61f01a7fd11
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-25 23:01:36 +0200

    add stub classes for Character and Integer

---
+++ src/jsava/lang/Character.js
@@ -0,0 +1,13 @@
+qx.Class.define( 'jsava.lang.Character', {
+    extend: jsava.lang.Object,
+    // TODO add Comparable interface
+    implement: [jsava.io.Serializable],
+
+    statics: {
+        // TODO a lot of constants to add
+    },
+
+    members: {
+        // TODO character methods
+    }
+} );
---
+++ src/jsava/lang/Integer.js
@@ -0,0 +1,17 @@
+qx.Class.define( 'jsava.lang.Integer', {
+    // TODO extend from Number
+    extend: jsava.lang.Object,
+    // TODO implement Comparable
+    implement: [],
+
+    statics: {
+        MIN_VALUE: 0x80000000,
+        MAX_VALUE: 0x7fffffff
+
+        // TODO add integer statics
+    },
+
+    members: {
+        // TODO add integer methods
+    }
+} );
