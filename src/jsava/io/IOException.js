da3cb46447327efdf2918c47d635ef5a2440c634
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-25 20:00:37 +0200

    added IOException

---
+++ src/jsava/io/IOException.js
@@ -0,0 +1,12 @@
+qx.Class.define( 'jsava.io.IOException', {
+    extend: jsava.lang.Exception,
+
+    construct: function () {
+        this.base.apply( this, Array.prototype.concat.call( arguments, Array.prototype.slice.call( arguments ) ) );
+    },
+
+    statics: {
+        /** @private */
+        serialVersionUID: 1
+    }
+} );
