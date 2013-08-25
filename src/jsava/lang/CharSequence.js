d78bd34df81562863ada84546f6844594bfe3202
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-25 19:56:21 +0200

    add CharSequence interface

---
+++ src/jsava/lang/CharSequence.js
@@ -0,0 +1,30 @@
+qx.Interface.define( 'jsava.lang.CharSequence', {
+    members: {
+        /**
+         * @return {Number}
+         */
+        length: function () {
+        },
+
+        /**
+         * @param {Number} index
+         * @return {String}
+         */
+        charAt: function (index) {
+        },
+
+        /**
+         * @param {Number} start
+         * @param {Number} end
+         * @return {jsava.lang.CharSequence}
+         */
+        subSequence: function (start, end) {
+        },
+
+        /**
+         * @return {String}
+         */
+        toString: function () {
+        }
+    }
+} );
