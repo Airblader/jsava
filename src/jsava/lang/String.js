c241cbb811a46bcd2aa9fe6a3bb62c98c0f14858
Ingo Bürk <ingo.buerk@tngtech.com>
2013-08-25 22:50:50 +0200

    add stub class for String

---
+++ src/jsava/lang/String.js
@@ -0,0 +1,9 @@
+qx.Class.define( 'jsava.lang.String', {
+    extend: jsava.lang.Object,
+    // TODO add Comparable interface
+    implement: [jsava.io.Serializable, jsava.lang.CharSequence],
+
+    members: {
+        // TODO missing everything right now :)
+    }
+} );
