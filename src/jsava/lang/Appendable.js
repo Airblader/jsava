8a2d4fb1a7b701dc8eb0d6a0e73eb1f0b9a70fb6
Ingo Bürk <ingo.buerk@tngtech.com>
2013-09-03 14:40:02 +0200

    format

--- src/jsava/lang/Appendable.js
+++ src/jsava/lang/Appendable.js
@@ -4,5 +4,6 @@ qx.Interface.define( 'jsava.lang.Appendable', {
      * @returns {jsava.lang.Appendable}
      * @throws {jsava.io.IOException}
      */
-    append: function () {}
+    append: function () {
+    }
 } );
