10d11105d5578721a9dd17dfe9b4edb10e1ad412
Ingo Bürk <ingo.buerk@tngtech.com>
2013-09-03 14:10:52 +0200

    renamed members in Throwable

--- src/jsava/lang/Throwable.js
+++ src/jsava/lang/Throwable.js
@@ -7,18 +7,18 @@ qx.Class.define( 'jsava.lang.Throwable', {
 
         var args = Array.prototype.slice.call( arguments );
         this.fillInStackTrace();
-        this.__cause = this;
+        this.cause = this;
 
         if( args.length === 1
             && qx.Class.isSubClassOf( args[0].constructor, jsava.lang.Throwable ) ) {
-            this.__detailMessage = (args[0] === null) ? null : args[0].toString();
-            this.__cause = args[0];
+            this.detailMessage = (args[0] === null) ? null : args[0].toString();
+            this.cause = args[0];
         } else if( args.length >= 1 && typeof args[0] === 'string' ) {
-            this.__detailMessage = args[0];
+            this.detailMessage = args[0];
 
             if( args.length === 2
                 && qx.Class.isSubClassOf( args[1].constructor, jsava.lang.Throwable ) ) {
-                this.__cause = args[1];
+                this.cause = args[1];
             }
         }
     },
@@ -29,13 +29,13 @@ qx.Class.define( 'jsava.lang.Throwable', {
     },
 
     members: {
-        __detailMessage: null,
+        detailMessage: null,
         /** @type jsava.lang.Throwable */
-        __cause: null,
-        __stackTrace: null,
+        cause: null,
+        stackTrace: null,
 
         getMessage: function () {
-            return this.__detailMessage;
+            return this.detailMessage;
         },
 
         getLocalizedMessage: function () {
@@ -43,18 +43,18 @@ qx.Class.define( 'jsava.lang.Throwable', {
         },
 
         getCause: function () {
-            return this.__cause;
+            return this.cause;
         },
 
         initCause: function (cause) {
-            if( this.__cause !== this ) {
+            if( this.cause !== this ) {
                 throw new jsava.lang.IllegalStateException( 'Can\'t overwrite cause' );
             }
             if( cause === this ) {
                 throw new jsava.lang.IllegalArgumentException( 'Self-causation is not permitted' );
             }
 
-            this.__cause = cause;
+            this.cause = cause;
             return this;
         },
 
@@ -67,20 +67,20 @@ qx.Class.define( 'jsava.lang.Throwable', {
         printStackTrace: function () {
             // TODO other method signatures
             if( console && console.error ) {
-                console.error( this.__stackTrace );
+                console.error( this.stackTrace );
             }
         },
 
         fillInStackTrace: function () {
-            this.__stackTrace = new Error().stack;
+            this.stackTrace = new Error().stack;
         },
 
         getStackTrace: function () {
-            return this.__stackTrace;
+            return this.stackTrace;
         },
 
         setStackTrace: function (stackTrace) {
-            this.__stackTrace = stackTrace;
+            this.stackTrace = stackTrace;
         }
     }
 } );
