8ae8dd82593e7b77a7897e7d5080f054c174358f
Ingo Bürk <ingo.buerk@tngtech.com>
2013-09-04 15:15:20 +0200

    enhance script to check super constructor calls

--- tools/checkSuperConstructorCalled.js
+++ tools/checkSuperConstructorCalled.js
@@ -1,19 +1,56 @@
 (function (root) {
+    // TODO run as Jasmine test for better visibility
+
     /**
      * This script will iterate over the root object and traverse through it to find all qooxdoo-defined classes
-     * that extend from jsava.lang.Object.
-     * Any class found will first be tried to instanciate using a no-arg constructor, but the super constructor
-     * will be proxied to verify whether it was called. If an exception is thrown, a fall-back solution comes into place
-     * where the constructor will be stringified and searched for a call to the super constructor.
+     * that extend from jsava.lang.Object and verify that the super constructor was called.
      *
      * The reason for this is that in Java, a class constructor can either manually call any of the available super
-     * constructors, or the compiler will call the no-arg, visibile super constructor automatically. This behavior
+     * constructors, or the compiler will call the no-arg, visible super constructor automatically. This behavior
      * differs from Javascript/qooxdoo, but is easy to forget. Therefore, this script will check for it.
      */
 
     'use strict';
 
-    var wrongClasses = [];
+    console.group( 'Verify that classes call super constructors' );
+    var errors = [];
+
+    var checkConstructor = function (Clazz) {
+        var oldBase = Clazz.constructor.base;
+        try {
+            if( !checkBySpying( Clazz, oldBase ) ) {
+                addClassToErrors( Clazz );
+            }
+        } catch( e ) {
+            console.info( 'Checking %s by spying failed. Using fallback.', Clazz );
+
+            if( !checkByToString( Clazz ) ) {
+                addClassToErrors( Clazz );
+            }
+        } finally {
+            Clazz.constructor.base = oldBase;
+        }
+    };
+
+    var checkStaticsAndMembers = function (Clazz) {
+        checkFields( Clazz );
+        if( typeof Clazz.prototype !== 'undefined' ) {
+            checkFields( Clazz.prototype );
+        }
+    };
+
+    var checkFields = function (Clazz) {
+        var fields = Object.keys( Clazz );
+        for( var j = 0; j < fields.length; j++ ) {
+            var current = Clazz[fields[j]];
+
+            if( isClass( current ) ) {
+                checkConstructor( current );
+                // TODO check fields for this class too (inner classes can have inner classes and so on)
+                //checkStaticsAndMembers( current );
+            }
+        }
+    };
 
     var checkBySpying = function (Clazz, base) {
         var verified = false;
@@ -29,46 +66,45 @@
 
     var checkByToString = function (Clazz) {
         // TODO can be improved
+        // usually the call is 'this.base( arguments ...)', but sometimes it is
+        // 'this.base.apply', so this currently covers both
         return Function.prototype.toString.call( Clazz.constructor ).indexOf( 'this.base' ) !== -1;
     };
 
     var addClassToErrors = function (Clazz) {
-        wrongClasses.push( Clazz.basename );
+        errors.push( Clazz.classname );
     };
 
-    (function traverse (obj) {
+    var isClass = function (obj) {
+        return typeof obj !== 'undefined' && obj !== null
+            && typeof obj.constructor !== 'undefined' && qx.Class.isSubClassOf( obj.constructor, jsava.lang.Object );
+    };
+
+    (function traversePackage (obj) {
         for( var i in obj ) {
             if( !obj.hasOwnProperty( i ) ) {
                 continue;
             }
 
             if( typeof obj[i] === 'object' ) {
-                traverse( obj[i] );
+                traversePackage( obj[i] );
             }
 
             var Clazz = obj[i];
-
-            if( !qx.Class.isSubClassOf( Clazz.constructor, jsava.lang.Object ) ) {
+            if( !isClass( Clazz ) ) {
                 continue;
             }
 
-            var oldBase = Clazz.constructor.base;
             try {
-                if( !checkBySpying( Clazz, oldBase ) ) {
-                    addClassToErrors( Clazz );
-                }
+                checkConstructor( Clazz );
+                checkStaticsAndMembers( Clazz );
             } catch( e ) {
-                if( !checkByToString( Clazz ) ) {
-                    addClassToErrors( Clazz );
-                }
-            } finally {
-                Clazz.constructor.base = oldBase;
+                console.error( 'Error while checking %s', Clazz );
             }
         }
     })( root );
 
-    // TODO what to do here?
-    if( wrongClasses.length !== 0 ) {
-        console.error( 'Found errors during super call validation: ' + wrongClasses.join( ', ' ) );
+    if( errors.length !== 0 ) {
+        throw new Error( 'Found errors during super call validation: ' + errors.join( ', ' ) );
     }
 })( jsava );
