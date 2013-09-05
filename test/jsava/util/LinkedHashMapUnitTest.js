ed8edca7ca9d8cda7d996af43522a2fcf411a542
Ingo Bürk <ingo.buerk@tngtech.com>
2013-09-05 19:37:14 +0200

    remove tests covered in base class

--- test/jsava/util/LinkedHashMapUnitTest.js
+++ test/jsava/util/LinkedHashMapUnitTest.js
@@ -23,44 +23,11 @@ describe( 'LinkedHashMap', function () {
         expect( map.get( 5 ) ).toBe( null );
     } );
 
-    it( 'size() counts the number of entries', function () {
-        expect( map.size() ).toBe( 0 );
-
-        map.put( 1, 1 );
-        expect( map.size() ).toBe( 1 );
-
-        map.put( 2, 2 );
-        map.put( 3, 3 );
-        expect( map.size() ).toBe( 3 );
-
-        map.remove( 3 );
-        map.remove( 2 );
-        expect( map.size() ).toBe( 1 );
-    } );
-
-    it( 'isEmpty() returns whether or not the map is empty', function () {
-        expect( map.isEmpty() ).toBe( true );
-
-        map.put( 0, 0 );
-        expect( map.isEmpty() ).toBe( false );
-
-        map.remove( 0 );
-        expect( map.isEmpty() ).toBe( true );
-    } );
-
-    it( 'toString() returns a correct string representation', function () {
-        expect( map.toString() ).toBe( '{}' );
-
-        map.put( 1, 10 );
-        map.put( 2, 20 );
-        expect( map.toString() ).toBe( '{1=10, 2=20}' );
-    } );
-
     describe( 'constructor', function () {
         // TODO constructors
     } );
 
-    describe( 'put() and get()', function () {
+    describe( 'get()', function () {
         it( 'work for a simple value', function () {
             var expectedKey = 1,
                 expectedValue = 'Hello World';
@@ -127,87 +94,7 @@ describe( 'LinkedHashMap', function () {
         } );
     } );
 
-    describe( 'putAll()', function () {
-        var otherMap;
-
-        beforeEach( function () {
-            otherMap = new LinkedHashMap();
-            otherMap.put( 1, 10 );
-            otherMap.put( 2, 20 );
-            otherMap.put( 3, 30 );
-        } );
-
-        it( 'can transfer all entries of another map into an empty map', function () {
-            map.putAll( otherMap );
-
-            expect( map.size() ).toBe( 3 );
-            expect( map.get( 1 ) ).toBe( 10 );
-            expect( map.get( 2 ) ).toBe( 20 );
-            expect( map.get( 3 ) ).toBe( 30 );
-        } );
-
-        it( 'can transfer all entries of another map into a non-empty map with no conflicts', function () {
-            map.put( 4, 40 );
-            map.putAll( otherMap );
-
-            expect( map.size() ).toBe( 4 );
-            expect( map.get( 1 ) ).toBe( 10 );
-            expect( map.get( 2 ) ).toBe( 20 );
-            expect( map.get( 3 ) ).toBe( 30 );
-            expect( map.get( 4 ) ).toBe( 40 );
-        } );
-
-        it( 'can transfer all entries of another map into a non-empty map with conflicts', function () {
-            map.put( 1, -10 );
-            map.put( 4, 40 );
-            map.putAll( otherMap );
-
-            expect( map.size() ).toBe( 4 );
-            expect( map.get( 1 ) ).toBe( 10 );
-            expect( map.get( 2 ) ).toBe( 20 );
-            expect( map.get( 3 ) ).toBe( 30 );
-            expect( map.get( 4 ) ).toBe( 40 );
-        } );
-    } );
-
-    describe( 'remove()', function () {
-        it( 'can remove an existing element', function () {
-            map.put( 1, 42 );
-
-            var removedElement = map.remove( 1 );
-            expect( removedElement ).toBe( 42 );
-            expect( map.get( 1 ) ).toBe( null );
-            expect( map.size() ).toBe( 0 );
-        } );
-
-        it( 'can handle trying to remove a non-existent element', function () {
-            expect( map.remove( 1 ) ).toBe( null );
-        } );
-
-        it( 'leaves other elements untouched', function () {
-            map.put( 1, 10 );
-            map.put( 2, 20 );
-            map.put( 3, 30 );
-
-            map.remove( 2 );
-
-            expect( map.size() ).toBe( 2 );
-            expect( map.get( 1 ) ).toBe( 10 );
-            expect( map.get( 2 ) ).toBe( null );
-            expect( map.get( 3 ) ).toBe( 30 );
-        } );
-    } );
-
-    describe( 'containsKey() and containsValue()', function () {
-        it( 'can verify the presence of a key', function () {
-            map.put( 1, 10 );
-            expect( map.containsKey( 1 ) ).toBe( true );
-        } );
-
-        it( 'can verify the absence of a key', function () {
-            expect( map.containsKey( 1 ) ).toBe( false );
-        } );
-
+    describe( 'containsValue()', function () {
         it( 'can verify the presence of a value', function () {
             map.put( 1, 10 );
             expect( map.containsValue( 10 ) ).toBe( true );
@@ -215,37 +102,6 @@ describe( 'LinkedHashMap', function () {
 
         it( 'can verify the absence of a value', function () {
             expect( map.containsValue( 10 ) ).toBe( false );
-        } );
-    } );
-
-    describe( 'equals()', function () {
-        it( 'returns true for the same object', function () {
-            expect( map.equals( map ) ).toBe( true );
-
-            map.put( 1, 10 );
-            expect( map.equals( map ) ).toBe( true );
-        } );
-
-        it( 'returns true for another map with same content', function () {
-            var otherMap = new LinkedHashMap();
-
-            expect( map.equals( otherMap ) ).toBe( true );
-            expect( otherMap.equals( map ) ).toBe( true );
-
-            map.put( 1, 10 );
-            otherMap.put( 1, 10 );
-
-            expect( map.equals( otherMap ) ).toBe( true );
-            expect( otherMap.equals( map ) ).toBe( true );
-        } );
-
-        it( 'returns false for different maps', function () {
-            var otherMap = new LinkedHashMap();
-            map.put( 1, 10 );
-
-            expect( map.equals( otherMap ) ).toBe( false );
-            expect( map.equals( null ) ).toBe( false );
-            expect( otherMap.equals( map ) ).toBe( false );
         } );
     } );
 
@@ -297,33 +153,6 @@ describe( 'LinkedHashMap', function () {
             }
 
             expect( counter ).toBe( map.size() );
-        } );
-    } );
-
-    describe( 'clone()', function () {
-        it( 'can clone an empty map', function () {
-            var otherMap = map.clone();
-            expect( otherMap.isEmpty() ).toBe( true );
-        } );
-
-        it( 'can clone a map with entries', function () {
-            map.put( 1, 10 );
-            map.put( 2, 20 );
-
-            var otherMap = map.clone();
-            expect( otherMap.size() ).toBe( 2 );
-            expect( otherMap.get( 1 ) ).toBe( 10 );
-            expect( otherMap.get( 2 ) ).toBe( 20 );
-        } );
-
-        it( 'creates a shallow copy', function () {
-            var valueMap = new LinkedHashMap();
-            map.put( 1, valueMap );
-
-            var otherMap = map.clone();
-            otherMap.get( 1 ).put( 2, 20 );
-
-            expect( valueMap.get( 2 ) ).toBe( 20 );
         } );
     } );
 
