df1dde3a8b5fa092c0724712e2c7f3e74d7d7678
Ingo Bürk <ingo.buerk@tngtech.com>
2013-09-09 20:21:00 +0200

    improved script to inherit statics

--- jsava/jsavaInit.js
+++ jsava/jsavaInit.js
@@ -1,14 +1,15 @@
 (function (window, undefined) {
     'use strict';
 
-    var qxInternals = ['$$implements', '$$extends', '$$flatImplements', '$$classtype'],
-    // TODO make this jsava-global
-        isLogEnabled = false;
+    var qxInternals = ['$$implements', '$$extends', '$$flatImplements', '$$classtype', '$$inheritedStatics'],
+        isLogEnabled = true;
 
     var inheritStatics = function (clazz) {
-        var hierarchy = [clazz];
-        while( hierarchy[hierarchy.length - 1].superclass !== qx.core.Object ) {
-            hierarchy.push( hierarchy[hierarchy.length - 1].superclass );
+        var hierarchy = [clazz],
+            currentSuperclass = null;
+        while( (currentSuperclass = hierarchy[hierarchy.length - 1].superclass) !== qx.core.Object
+            && typeof hierarchy[hierarchy.length - 1].$$inheritedStatics === 'undefined' ) {
+            hierarchy.push( currentSuperclass );
         }
 
         for( var i = hierarchy.length - 1; i > 0; i-- ) {
@@ -19,22 +20,39 @@
                 // TODO private members should not be inherited
                 if( !baseClass.hasOwnProperty( staticMember ) && qxInternals.indexOf( staticMember ) === -1 ) {
                     baseClass[staticMember] = superClass[staticMember];
-                    isLogEnabled && console.info( 'inherited ' + superClass.classname + '.' + staticMember );
+                    isLogEnabled && console.info( baseClass.classname + ': inherited '
+                        + superClass.classname + '.' + staticMember );
                 }
             }
+
+            baseClass.$$inheritedStatics = true;
         }
     };
 
+    /**
+     * @param className
+     * @param properties
+     * @returns {qx.Class|Class|*}
+     */
     var defineClass = function (className, properties) {
-        isLogEnabled && console.group( 'Defining new class ' + className );
+        isLogEnabled && console.group(
+            className === null ? 'Defining new anonymous class' : 'Defining new class ' + className );
 
-        var clazz = qx.Class.define( className, properties );
-        inheritStatics( clazz );
+        try {
+            var clazz = qx.Class.define( className, properties );
+            inheritStatics( clazz );
+        } finally {
+            isLogEnabled && console.groupEnd();
+        }
 
-        isLogEnabled && console.groupEnd();
         return clazz;
     };
 
+    /**
+     * @param interfaceName
+     * @param properties
+     * @returns {qx.Interface|Interface|*}
+     */
     var defineInterface = function (interfaceName, properties) {
         return qx.Interface.define( interfaceName, properties );
     };
